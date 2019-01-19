7a29cae97b8a0a679d37b015a4ec87997876c2bc
Jack Nosek <jnosek@gmail.com>
2019-01-19 16:30:41 -0500

    Copy now renames local file Moves unknow files to failed folder

--- import-photos.ps1
+++ import-photos.ps1
@@ -2,6 +2,8 @@ param
 (
     $SourceFolder = ".", 
     $DestinationFolder = $(throw "-DestinationFolder is required."),
+    # "Move" will rename the file and move it to the destination
+    # "Copy" will rename the file in the Source Folder and Copy it with the new name to the destination
     [ValidateSet("Move","Copy")]
     [System.String] $Operation = "Copy",
     [System.String] $SourceDateTimeSelector = "IMG_(.*)\.jpg",
@@ -9,16 +11,20 @@ param
     [System.String] $DestinationDateTimeFormat = "yyyyMMdd-HHmmss"
 )
 
-# if destination does not exist
+$failedFolder = $SourceFolder + "\Failed";
+
+# if failed folder does not exist, create it
+if(-not (Test-Path -Path $failedFolder)) {
+    New-Item -Path $failedFolder -ItemType Directory | Out-Null;
+}
+
+# if destination does not exist, create it
 if(-not (Test-Path -Path $DestinationFolder)) {
     New-Item -Path $DestinationFolder -ItemType Directory | Out-Null;
 }
 
-# declare last file for saving copy state
-$lastFile = $null;
-
 # get files in folder ordered by name ascending
-$files = Get-ChildItem -Path $SourceFolder | Sort-Object -Property FullName;
+$files = Get-ChildItem -Path $SourceFolder -File | Sort-Object -Property FullName;
 
 # for each file in the folder
 foreach($file in $files) {
@@ -32,48 +38,47 @@ foreach($file in $files) {
         # parse into a datetime object
         $dateValue = [DateTime]::ParseExact($dateString, $SourceDateTimeFormat, $null);
 
-        # build newFilePath with year
-        $newFilePath = $DestinationFolder + $dateValue.Year.ToString();
+        # build newFilePath with year and month
+        $newFilePath = $DestinationFolder + $dateValue.Year.ToString() + "\" + $dateValue.Month.ToString("00");
 
-        if(-not (Test-Path -Path $newFilePath))
-        {
-            New-Item -Path $newFilePath -ItemType Directory | Out-Null;
-        }
-
-        # build newFilePath with month
-        $newFilePath = $newFilePath + "\" + $dateValue.Month.ToString("00");
-
+        # if directory does not exist create it
         if(-not (Test-Path -Path $newFilePath)) {
             New-Item -Path $newFilePath -ItemType Directory | Out-Null;
         }
 
         # create a new filename based off the datetime object
-        $newFilePath = $newFilePath + "\" + $dateValue.ToString([string] $DestinationDateTimeFormat) + $file.Extension;
+        $newFileName =  $dateValue.ToString([string] $DestinationDateTimeFormat) + $file.Extension;
+        $newFilePath = $newFilePath + "\" + $newFileName;
 
         # TODO: what if the file already exists
 
-        # write transaction output
-        Write-Host("{0} -> {1}" -f $file.Name, $newFilePath);
-
         # copy file
         if($Operation -eq "Copy") {
+            # write transaction output
+            Write-Host("{0} <-> {1}" -f $file.Name, $newFilePath);
+
+            # rename current file
+            $file =Rename-Item -Path $file.FullName -NewName $newFileName -PassThru;
+            
             Copy-Item -Path $file.FullName -Destination $newFilePath;    
         }
         # move file
         elseif($Operation -eq "Move") {
+            # write transaction output
+            Write-Host("{0} -> {1}" -f $file.Name, $newFilePath);
+
             Move-Item -Path $file.FullName -Destination $newFilePath; 
         }
         # unknown operation value
         else {
             throw ("Unknown Operation Value: {0}" -f $Operation);
         }
-        
-        # save last selected file that was processed
-        $lastFile = $file;
     }
-}
+    # else, move files that do not match to the failed folder
+    else {
+        # write transaction output
+        Write-Host("! {0}" -f $file.Name);
 
-# if we are copying save last file
-if($Operation -eq "Copy") {
-    # TODO: save copy state
+        Move-Item -Path $file.FullName -Destination $failedFolder;
+    }
 }
