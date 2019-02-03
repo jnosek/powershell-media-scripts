80c0e708d2cb6d9ac1ec156e016f3c2847b70470
Jack Nosek <jnosek@gmail.com>
2019-02-03 00:48:03 -0500

    refactored to allow passing different source types

--- import-photos.ps1
+++ import-photos.ps1
@@ -8,14 +8,64 @@ param
     [System.String] $Operation = "Copy"
 )
 
-$sourceFileTypeSelector = "*.jpg";
-$sourceDateTimeSelector = "^IMG_([0-9]{8}_[0-9]{6})\.jpg$";
-$sourceDateTimeFormat = "yyyyMMdd_HHmmss";
+class Source {
+    [string] $Folder;
+    [string] $FileTypeSelector;
+    [string] $DateTimeRegex;
+    [string] $DateTimeFormat;
 
-$destinationDateTimeFormat = "yyyyMMdd-HHmmss";
-$destinationRegExFormat = "^[0-9]{8}-[0-9]{6}(-[0-9]*)?\.jpg$";
+    Source($folder, $fileType, $dateTimeRegex, $dateTimeFormat){
+        $this.Folder = $folder;
+        $this.FileTypeSelector = $fileType;
+        $this.DateTimeRegex = $dateTimeRegex;
+        $this.DateTimeFormat = $dateTimeFormat;
+    }
+}
 
-$failedFolder = $SourceFolder + "\Failed";
+class NewFile {
+    [string] $BaseName;
+    [string] $Name;
+    [string] $Path;
+
+    NewFile($baseName, $path) {
+        $this.BaseName = $baseName;
+        $this.Path = $path;
+    }
+
+    [string] FullPath() {
+        return $this.Path + "\" + $this.Name;
+    }
+
+    [void] SetNameWithExtension([string] $ext) {
+        $this.Name = $this.BaseName + $ext;
+    }
+}
+
+$androidImageSource = [Source]::new(
+    $SourceFolder,
+    "*.jpg",
+    # select files like:
+    # IMG_########_######.jpg
+    # IMG_########_######_#.jpg
+    "^IMG_([0-9]{8}_[0-9]{6})(?:_[0-9]*)?\.jpg$",
+    "yyyyMMdd_HHmmss");
+
+$androidVideoSource = [Source]::new(
+    $SourceFolder,
+    "*.mp4",
+    # select files like:
+    # IMG_########_######.jpg
+    # IMG_########_######_#.jpg
+    "^VID_([0-9]{8}_[0-9]{6})(?:_[0-9]*)?\.mp4$",
+    "yyyyMMdd_HHmmss");
+
+$destination = [PSCustomObject]@{
+    Folder = $DestinationFolder;
+    DateTimeFormat = "yyyyMMdd-HHmmss";
+    RegExFormat = "^[0-9]{8}-[0-9]{6}(-[0-9]*)?\.jpg$";
+}
+
+$failedFolder = $source.Folder + "\Failed";
 
 function fileFailure($failedFile)
 {
@@ -25,93 +75,100 @@ function fileFailure($failedFile)
      Move-Item -Path $failedFile.FullName -Destination $failedFolder;
 }
 
+function processSource([Source] $source) {
+    # get files in folder ordered by name ascending
+    $files = @(Get-ChildItem -Path $source.Folder -File -Filter $source.FileTypeSelector);
+
+    # for each file in the folder
+    foreach($currentFile in $files) {
+
+        # if the file name matches the selector
+        if($currentFile.Name -match $source.DateTimeRegex) {
+
+            # retrieve the date string selected by the expression
+            $dateString = $Matches[1];
+
+            [DateTime] $dateValue = New-Object DateTime;
+            
+            # if we can parse the datetime
+            if([DateTime]::TryParseExact(
+                $dateString, 
+                $source.DateTimeFormat, 
+                [System.Globalization.CultureInfo]::InvariantCulture, 
+                [System.Globalization.DateTimeStyles]::None,
+                [ref] $dateValue))
+            {
+                $newFile = [NewFile]::new(
+                    # create a new basename off the datetime object
+                    $dateValue.ToString([string] $destination.DateTimeFormat),
+                    # build newFilePath with year and month
+                    $destination.Folder + $dateValue.Year.ToString() + "\" + $dateValue.Month.ToString("00")
+                );
+
+                # set file extension
+                $newFile.SetNameWithExtension($currentFile.Extension); 
+
+                # if directory does not exist create it
+                if(-not (Test-Path -Path $newFile.Path)) {
+                    New-Item -Path $newFile.Path -ItemType Directory | Out-Null;
+                }
+
+                # if the file already exists
+                if(Test-Path -Path $newFile.FullPath())
+                {
+                    $matchFileRegex = "^" + $newFile.BaseName + "(.*)\.jpg$";
+                    # find count of files that starts the same
+                    $matchFiles = @(Get-ChildItem -Path $newFile.Path -File | Where-Object { $_.Name -match $matchFileRegex });
+                    
+                    # set fileName to - count + 1
+                    $newFile.BaseName = $newFile.BaseName + "-" + ($matchFiles.Length + 1);
+                    $newFile.SetNameWithExtension($currentFile.Extension);
+                }
+
+                # copy file
+                if($Operation -eq "Copy") {
+                    # write transaction output
+                    Write-Host("{0} <-> {1}" -f $currentFile.Name, $newFile.FullPath());
+
+                    # rename current file
+                    $currentFile = Rename-Item -Path $currentFile.FullName -NewName $newFile.Name -PassThru;
+                    
+                    Copy-Item -Path $currentFile.FullName -Destination $newFile.Path;    
+                }
+                # move file
+                elseif($Operation -eq "Move") {
+                    # write transaction output
+                    Write-Host("{0} -> {1}" -f $currentFile.Name, $newFile.FullPath());
+
+                    Move-Item -Path $currentFile.FullName -Destination $newFile.FullPath(); 
+                }
+                # unknown operation value
+                else {
+                    throw ("Unknown Operation Value: {0}" -f $Operation);
+                }
+            }      
+        }
+        # else, if the filename matches our destination format
+        elseif($currentFile.Name -match $destination.RegExFormat) {
+            # skip
+        }
+        # else, move files that do not match to the failed folder
+        else {
+            fileFailure $currentFile;
+        }
+    }
+}
+
 # if failed folder does not exist, create it
 if(-not (Test-Path -Path $failedFolder)) {
     New-Item -Path $failedFolder -ItemType Directory | Out-Null;
 }
 
 # if destination does not exist, create it
-if(-not (Test-Path -Path $DestinationFolder)) {
-    New-Item -Path $DestinationFolder -ItemType Directory | Out-Null;
+if(-not (Test-Path -Path $destination.Folder)) {
+    New-Item -Path $destination.Folder -ItemType Directory | Out-Null;
 }
 
-# get files in folder ordered by name ascending
-$files = @(Get-ChildItem -Path $SourceFolder -File -Filter $sourceFileTypeSelector);
+processSource($androidImageSource);
 
-# for each file in the folder
-foreach($file in $files) {
-
-    # if the file name matches the selector
-    if($file.Name -match $sourceDateTimeSelector) {
-
-        # retrieve the date string selected by the expression
-        $dateString = $Matches[1];
-
-        [DateTime] $dateValue = New-Object DateTime;
-        
-        # if we can parse the datetime
-        if([DateTime]::TryParseExact(
-            $dateString, 
-            $sourceDateTimeFormat, 
-            [System.Globalization.CultureInfo]::InvariantCulture, 
-            [System.Globalization.DateTimeStyles]::None,
-            [ref] $dateValue))
-        {
-            # build newFilePath with year and month
-            $newFilePath = $DestinationFolder + $dateValue.Year.ToString() + "\" + $dateValue.Month.ToString("00");
-
-            # if directory does not exist create it
-            if(-not (Test-Path -Path $newFilePath)) {
-                New-Item -Path $newFilePath -ItemType Directory | Out-Null;
-            }
-
-            # create a new filename based off the datetime object
-            $newFileBaseName = $dateValue.ToString([string] $destinationDateTimeFormat);
-            $newFileName = $newFileBaseName + $file.Extension;
-            $newFileFullName = $newFilePath + "\" + $newFileName;
-
-            # if the file already exists
-            if(Test-Path -Path $newFileFullName)
-            {
-                $matchFileRegex = "^" + $newFileBaseName + "(.*)\.jpg$";
-                # find count of files that starts the same
-                $matchFiles = @(Get-ChildItem -Path $newFilePath -File | Where-Object { $_.Name -match $matchFileRegex });
-                
-                # set fileName to - count + 1
-                $newFileBaseName = $newFileBaseName + "-" + ($matchFiles.Length + 1);
-                $newFileName = $newFileBaseName + $file.Extension;
-                $newFileFullName = $newFilePath + "\" + $newFileName;
-            }
-
-            # copy file
-            if($Operation -eq "Copy") {
-                # write transaction output
-                Write-Host("{0} <-> {1}" -f $file.Name, $newFileFullName);
-
-                # rename current file
-                $file = Rename-Item -Path $file.FullName -NewName $newFileName -PassThru;
-                
-                Copy-Item -Path $file.FullName -Destination $newFilePath;    
-            }
-            # move file
-            elseif($Operation -eq "Move") {
-                # write transaction output
-                Write-Host("{0} -> {1}" -f $file.Name, $newFileFullName);
-
-                Move-Item -Path $file.FullName -Destination $newFileFullName; 
-            }
-            # unknown operation value
-            else {
-                throw ("Unknown Operation Value: {0}" -f $Operation);
-            }
-        }      
-    }
-    # else, if the filename matches our destination format
-    elseif($file.Name -match $destinationRegExFormat) {
-        # skip
-    }
-    # else, move files that do not match to the failed folder
-    else {
-        fileFailure $file;
-    }
-}
+processSource($androidVideoSource);
